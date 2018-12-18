beb333a479a29b9cf5719586d2d3398b0e682cc1
normanrs <normanrs@live.com>
2018-12-18 11:36:36 -0700

    Adding user attributes
    Added uid and name obtained from Google to user attributes. Tested.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :google_token, presence: true
+  validates :uid, presence: true
+  validates :name, presence: true
   has_many  :parkings
 
 end
