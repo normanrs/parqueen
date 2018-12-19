5ee35d66766db98eec2c7d5b31f3143be4627c17
Meghan Stang <mstang15@gmail.com>
2018-12-18 17:41:51 -0700

    add commented out test for later

--- spec/feature/user/user_can_log_in_spec.rb
+++ spec/feature/user/user_can_log_in_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
   context 'as a user' do
-    it 'I can log in with google' do
-      user = User.create(uid: '1234', token: '4321', email: 'example@gmail.com', name: "John Smith")
+    xit 'I can log in with google' do
+      user = User.create(uid: '1234', google_token: '4321', email: 'example@gmail.com', name: "John Smith")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit '/'
       click_link "Log in with Google"
 
       expect(user.uid).to eq('1234')
-      expect(user.token).to eq('4321')
+      expect(user.google_token).to eq('4321')
       expect(user.name).to eq('John Smith')
       expect(user.email).to eq('example@gmail.com')
       expect(current_path).to eq('/home')
