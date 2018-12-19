392943abe351000a96ba50aa92fa07498acf273e
normanrs <normanrs@live.com>
2018-12-19 13:07:13 -0700

    Can get location coordinate
    Now have a functioning button to obtain location coordinates from the browser via jquery.

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -6,22 +6,22 @@ class Parking < ApplicationRecord
   validates_inclusion_of :legal, in: [true, false]
   belongs_to             :user
 
-	attr_reader            :street_name,
-  							         :start_cross_street,
+  attr_reader            :street_name,
+                         :start_cross_street,
                          :end_cross_street,
                          :side_of_street,
                          :legal
 
-	def initialize(data)
-		@street_name        = data[:properties][:metadata][:street_name]
-		@start_cross_street = data[:properties][:metadata][:start_street_name]
-		@end_cross_street   = data[:properties][:metadata][:end_street_name]
-		@side_of_street     = data[:properties][:metadata][:side_of_street]
+  def initialize(data)
+  @street_name        = data[:properties][:metadata][:street_name]
+  @start_cross_street = data[:properties][:metadata][:start_street_name]
+  @end_cross_street   = data[:properties][:metadata][:end_street_name]
+  @side_of_street     = data[:properties][:metadata][:side_of_street]
     if data[:properties][:uses][:use] == "park"
-      @legal            = true
+      @legal          = true
     else
-      @legal            = false
+      @legal          = false
     end
-	end
+  end
 
 end
