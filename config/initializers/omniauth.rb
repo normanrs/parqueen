3c806a9d42511d4bba950f85f4d5a0df5e3406ac
Meghan Stang <mstang15@gmail.com>
2018-12-18 17:47:17 -0700

    removes scope from config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,11 +3,5 @@
 # end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    # :scope => ['contacts','plus.login','plus.me','email','profile'],
-    :prompt => "select_account",
-    :image_aspect_ratio => "square",
-    :image_size => 50,
-    :access_type => 'offline'
-  }
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
