6066c3c1b48d104998eaf47c74b6aa614669b3b5
Meghan Stang <mstang15@gmail.com>
2018-12-18 17:38:43 -0700

    add method to create users from auth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   validates :google_token, presence: true
   has_many  :parkings
 
+  def self.update_or_create(auth)
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+      uid: auth[:uid],
+      email: auth[:info][:email],
+      name: auth[:info][:name],
+      google_token: auth[:credentials][:token]
+    }
+    user.save!
+    user
+  end
 end
