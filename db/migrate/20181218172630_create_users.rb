120cfafa9a222e71421a386a2f023bef18e487ef
Meghan Stang <mstang15@gmail.com>
2018-12-18 17:41:04 -0700

    add user table

--- db/migrate/20181218172630_create_users.rb
+++ db/migrate/20181218172630_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :email
       t.string :google_token
+      t.string :uid
+      t.string :name
 
       t.timestamps
     end
