fef60e0517da1326e091616875256abd5dd626c9
Meghan Stang <mstang15@gmail.com>
2018-12-23 11:18:07 -0700

    add delayed_jobs table and add phone number and contact boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_174642) do
+ActiveRecord::Schema.define(version: 2018_12_23_031343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
 
   create_table "parkings", force: :cascade do |t|
     t.point "coord"
@@ -37,6 +52,8 @@ ActiveRecord::Schema.define(version: 2018_12_18_174642) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "phone_number"
+    t.boolean "contact", default: true
   end
 
   add_foreign_key "parkings", "users"
