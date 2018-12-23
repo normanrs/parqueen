e4707f0e731a59140513b8253c24bd2508089387
Meghan Stang <mstang15@gmail.com>
2018-12-23 11:15:26 -0700

    add routes for updating users notifications and sending/setting notifications and texts

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   get '/home', to: 'home#show'
   get '/parking', to: "parking#show", as: 'parking'
   get '/logout', to: 'sessions#destroy'
+  post '/notifications/:id', to: 'users#update', as: 'notifications'
+  get '/notifications/:id', to: 'users#update'
+  post '/text', to: 'texts#create'
+
+  resources :users, only: [:update, :create]
+
+
 end
