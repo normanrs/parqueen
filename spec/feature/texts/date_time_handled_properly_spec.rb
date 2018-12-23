88d2fcbf2084fd4330ef988912434bce713c98a0
Meghan Stang <mstang15@gmail.com>
2018-12-23 12:24:44 -0700

    starts test for texts, but need to wait until parking saved in db

---
+++ spec/feature/texts/date_time_handled_properly_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# describe 'when date/time comes in from expiration' do
+#   it 'it can properly parse time stamps' do
+#     user = User.create(uid: '1234', google_token: '4321', email: 'example@gmail.com', name: "John Smith")
+#     phone = "7175565567"
+#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+#
+#     visit '/home'
+#
+#     click_on "Park Here"
+#
+#
+#   end
+# end
