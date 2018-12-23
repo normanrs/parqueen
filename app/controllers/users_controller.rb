b39a20402158c481585d62b125679265bc615ecb
Meghan Stang <mstang15@gmail.com>
2018-12-23 11:14:49 -0700

    add update method to allow users to toggle receiving notifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
-class UsersController<ApplicationController
+class UsersController <ApplicationController
+
+  def update
+    user = User.find(params[:id])
+    if params[:commit] == "Save My Number"
+      user[:phone_number] = params[:user][:phone_number]
+      user.save
+      flash[:success] = "Your phone number is now saved."
+      redirect_to home_path
+    elsif params[:commit] == "Opt Out Of Notifications"
+      user.contact = false
+      user.save
+      flash[:success] = "You've opted out of text notifications.\nYou can change that at anytime by visiting the dropdown menu."
+      redirect_to home_path
+    else
+      user.toggle :contact
+      user.save
+      redirect_to home_path
+    end
+  end
+
 
 end
