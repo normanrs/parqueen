76d9b7d108f4b9982f02ba034553547e718ef550
Meghan Stang <mstang15@gmail.com>
2018-12-27 17:14:14 -0500

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,19 @@ class UsersController <ApplicationController
     user = User.find(params[:id])
     if params[:commit] == "Save My Number"
       user[:phone_number] = params[:user][:phone_number]
-      current_user[:phone_number] = params[:user][:phone_number]
+      user.save
       flash[:success] = "Your phone number is now saved."
       redirect_to home_path
     elsif params[:commit] == "Opt Out Of Notifications"
       user.contact = false
-      current_user.contact = false
+      user.save
       flash[:success] = "You've opted out of text notifications.\nYou can change that at anytime by visiting the dropdown menu."
       redirect_to home_path
     else
       user.toggle :contact
-      current_user.toggle :contact
+      user.save
       redirect_to home_path
     end
   end
-
 
 end
--- spec/feature/texts/date_time_handled_properly_spec.rb
+++ spec/feature/texts/date_time_handled_properly_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
-# describe 'when date/time comes in from expiration' do
-#   it 'it can properly parse time stamps' do
-#     user = User.create(uid: '1234', google_token: '4321', email: 'example@gmail.com', name: "John Smith")
-#     phone = "7175565567"
-#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-#
-#     visit '/home'
-#
-#     click_on "Park Here"
-#
-#
-#   end
-# end
+describe 'when user parks' do
+  it 'it can show option to set expiration time' do
+    user = User.create(uid: '1234', google_token: '4321', email: 'example@gmail.com', name: "John Smith", phone_number: 1234567890, contact: true)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/home'
+
+    click_on "Park Here"
+
+    if location == valid
+      expect(page).to have_content("Set Expiration")
+
+      click_on "Set Expiration"
+      expect(current_path).to eq('/')
+    elsif location != valid
+      expect(page).to_not have_content("Set Expiration")
+    end
+  end
+
+  it 'and parks in valid location, time is parsed correctly' do
+    
+  end
+end
