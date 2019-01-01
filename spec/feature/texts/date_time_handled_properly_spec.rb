c227b8f061344d6c6a3a5b2bea19a153044399c0
Meghan Stang <mstang15@gmail.com>
2019-01-01 15:50:21 -0700

    tests expiration and initial messages

--- spec/feature/texts/date_time_handled_properly_spec.rb
+++ spec/feature/texts/date_time_handled_properly_spec.rb
@@ -18,4 +18,15 @@ describe 'when user parks' do
 
     expect(page).to_not have_button("Set Expiration")
   end
+
+  it 'handles time correctly with TwilioFacade' do
+    user = User.create(uid: '1234', google_token: '4321', email: 'example@gmail.com', name: "John Smith", phone_number: 1234567890, contact: false)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    twilio = TwilioFacade.new(user,Time.now)
+    time = twilio.expiration_time
+
+    expect(twilio.initial_message).to eq("John Smith, thank you for using ParQueen. We will notify you fifteen minutes before your parking expires.")
+    expect(twilio.expiration_message).to eq("Your parking will expire in 15 minutes. You have until #{time} to move your vehicle.")
+  end
 end
